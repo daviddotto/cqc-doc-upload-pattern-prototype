e47f522f28b311978e016bf2ade847262cbc8bec
David Todd <david.todd@cqc.org.uk>
2019-11-21 11:47:35 +0000

    Added success banner

--- app/routes.js
+++ app/routes.js
@@ -3,6 +3,13 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+// Save page load time to 'time' variable
+router.all('*', (req, _, next) => {
+    req.session.data['time'] = new Date().getTime()
+    req.session.data['fileUploaded'] = false
+    next()
+})
+
 router.post('/correct-file', (req, res) => {
     const isFileCorrect = req.session.data['doc-correct'] == 'yes'
     if (isFileCorrect) {
