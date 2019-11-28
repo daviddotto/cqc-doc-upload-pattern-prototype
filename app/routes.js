90d9bd8cb68939d1ed6d011403ed63567749b015
daviddotto <me@david.to>
2019-11-28 11:38:28 +0000

    Updates to single-file journey

--- app/routes.js
+++ app/routes.js
@@ -5,19 +5,29 @@ const router = express.Router()
 
 // Save page load time to 'time' variable
 router.all('*', (req, _, next) => {
-    req.session.data['time'] = new Date().getTime()
-    req.session.data['fileUploaded'] = false
-    req.session.data['fileProcessing'] = false
-    next()
+	req.session.data['time'] = new Date().getTime()
+	req.session.data['fileUploaded'] = false
+	req.session.data['fileProcessing'] = false
+	req.session.data['fromCheck'] = false
+	next()
 })
 
 router.post('/correct-file', (req, res) => {
-    const isFileCorrect = req.session.data['doc-correct'] == 'yes'
-    if (isFileCorrect) {
-        res.redirect('index')
-    } else {
-        res.redirect('upload-document')
-    }
+	const isFileCorrect = req.session.data['doc-correct'] == 'yes'
+	if (isFileCorrect) {
+		res.redirect('multiple/doc-list')
+	} else {
+		res.redirect('multiple/upload-document')
+	}
+})
+
+router.post('/correct-cv', (req, res) => {
+	const isFileCorrect = req.session.data['doc-correct'] == 'yes'
+	if (isFileCorrect) {
+		res.redirect('single-file/check-your-answers')
+	} else {
+		res.redirect('single-file/cv-upload?fromCheck=true')
+	}
 })
 
 module.exports = router
